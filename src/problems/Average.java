569aea6136a62f0c28004fc14298e4b2231be4fe
xJam-byte <abylaimuratbayev@gmail.com>
2025-04-02 22:14:31 +0500

    second problem

---
+++ src/problems/Average.java
@@ -0,0 +1,33 @@
+package problems;
+
+import java.util.Scanner;
+
+/**
+ * This method calculates the average of an array using recursion.
+ *
+ * Approach:
+ * - The function accumulates the sum and divides by the number of elements.
+ * - Uses the formula: (previous sum * (n - 1) + current value) / n.
+ *
+ * Time Complexity: O(n), where n is the number of elements.
+ *
+ * param arr The array of integers.
+ * param n The number of elements in the array.
+ * @return The average of the array elements.
+ */
+public class Average {
+    public static double findAverage(int[] arr, int n) {
+        if (n == 1) return arr[0];
+        return (findAverage(arr, n - 1) * (n - 1) + arr[n - 1]) / n;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = scanner.nextInt();
+
+        System.out.printf("%.2f\n", findAverage(arr, n));
+        scanner.close();
+    }
+}

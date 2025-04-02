794b76c62d5ee97f6e5d8a8e5a8aa083739a40a6
xJam-byte <abylaimuratbayev@gmail.com>
2025-04-02 22:57:10 +0500

    sixth problem

---
+++ src/problems/Power.java
@@ -0,0 +1,33 @@
+package problems;
+
+import java.util.Scanner;
+
+/**
+ *
+ * Approach:
+ * - Base case: Any number raised to the power of 0 is 1.
+ * - If the exponent is even: a^b = (a^(b/2)) * (a^(b/2)).
+ * - If the exponent is odd: a^b = a * (a^(b-1)).
+ *
+ * Time Complexity: O(log b), since we reduce the exponent by half in each step.
+ * Space Complexity: O(log b) (due to recursion stack).
+ *
+ * param a base number.
+ * param b The exponent.
+ * @return The value of a raised to the power of b.
+ */
+public class Power {
+    public static long power(int a, int b) {
+        if (b == 0) return 1;
+        long half = power(a, b / 2);
+        return (b % 2 == 0) ? half * half : half * half * a;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(power(a, b));
+        scanner.close();
+    }
+}
